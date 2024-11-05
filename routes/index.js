e73d884da3ac9cf37334d8db901976c4c7933ac6
chloehoang23 <chloe.hoang1@ontariotechu.net>
2024-11-05 00:29:22 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Chloe' });
-});
-
-/* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Chloe' }); // Change 'index' to name of ejs applied
+  res.render('index', { title: 'Home' });
 });
 
 /* GET about page. */
-router.get('#about', function(req, res, next) {
-  res.render('index', { title: 'About' });
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
 });
 
 /* GET product page. */
-router.get('#project', function(req, res, next) {
+router.get('/#project', function(req, res, next) {
   res.render('index', { title: 'Projects' });
 });
 
 /* GET contact page. */
-router.get('#contact', function(req, res, next) {
+router.get('/#contact', function(req, res, next) {
   res.render('index', { title: 'Contact' });
 });
 
